d795c2e0147da749094548c2fd121fbd445f3ebf
Hugh Kennedy <hughskennedy@gmail.com>
2013-08-11 15:38:18 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# object-pool #
+
+A generic object pool which you can use for reducing object allocations when
+you're creating and removing lots of objects in quick succession. Or just to
+recycle objects, which is handy in cases such as game development.
+
+## Installation ##
+
+``` javascript
+npm install object-pool
+```
+
+## Usage ##
+
+### `pool = require('object-pool')([options])` ###
+
+Creates a new object pool, taking the following arguments:
+
+* `init`: a factory method which should return a freshly created object.
+* `enable`: called on an object when it's added to the pool.
+* `disable`: called on an object when it's being removed from the pool.
+* `key`: this module stores a reference to each node on each object. Use
+  this option to chance the key it uses. Defaults to `__pool_node__`.
+
+### `pool.create()` ###
+
+Returns a fresh object from the pool. If there aren't any objects left in the
+reserve, this will call `init` to create a new object and then `enable` on
+the object to get it set up. Otherwise, this will retrieve an object from the
+reserve and just call `enable` on it.
+
+### `pool.remove(object)` ###
+
+Removes an object from the pool, adding it to a reserve list to use later and
+calling `disable` on it.
+
+### `pool.clean()` ###
+
+Empties the "reserve" list of leftover objects.
---
+++ index.js
@@ -0,0 +1,76 @@
+var LinkedList = require('circular-list')
+var Node = LinkedList.Node
+
+module.exports = Pool
+
+function noop(){}
+function defaultInit() {
+  return {}
+}
+
+function Pool(opts) {
+  if (!(this instanceof Pool)) {
+    return new Pool(opts)
+  }
+
+  opts = opts || {}
+  this.reserve = new LinkedList
+  this.list = new LinkedList
+
+  this.key = opts.key || '__pool_node__'
+  this.disable = opts.disable || noop
+  this.enable = opts.enable || noop
+  this.init = opts.init || defaultInit
+}
+
+Pool.prototype.create = function() {
+  if (this.reserve.first === null) {
+    var object = this.init()
+    var node = new Node(object)
+    object[this.key] = node
+  } else {
+    var node = this.reserve.first
+    var object = node.data
+    this.reserve.remove(node)
+  }
+
+  this.enable(object)
+  this.list.append(node)
+
+  return object
+}
+
+Pool.prototype.remove = function(entity) {
+  this.disable(entity)
+  this.list.remove(entity[this.key])
+  this.reserve.append(entity[this.key])
+  return this
+}
+
+Pool.prototype.each = function(cb) {
+  var p = this.list.first
+  var n = this.list.length
+
+  while (n--) {
+    cb(p.data)
+    p = p.next
+  }
+}
+
+Pool.prototype.clean = function() {
+  this.reserve.length = 0
+  this.reserve.first = null
+  this.reserve.last = null
+}
+
+var pool = Pool(function init() {
+  return { active: false }
+}, {
+  key: '__pool_node__',
+  enable: function(entity) {
+    entity.active = true
+  },
+  disable: function(entity) {
+    entity.active = false
+  }
+})
